b3f6f9b140412204ab33365e592c468a1fe0bca3
chauid <dlfdl28@naver.com>
2024-06-02 13:03:37 +0900

    tt

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,4 @@
 <script lang="ts">
-  import NavBar from '$lib/components/layouts/NavBar.svelte';
-  import { page } from '$app/stores';
-  import type { ProductType } from '$lib/server/modules/products';
-  import type { ActionData, PageServerData } from './$types';
-  import { onMount } from 'svelte';
-
-  export let data: PageServerData;
-  export let form: ActionData;
-  const pprops: ProductType = data.page_server.props;
-
   let stateValue: number = 0;
   function addState() {
     stateValue++;
@@ -20,41 +10,8 @@
 <svelte:head>
   <title>Intro</title>
 </svelte:head>
-<NavBar />
-<h1 class="text-5xl">Welcome to SvelteKit</h1>
-<ul>
-  <!-- <li>{data.layout.a}</li>
-	<li>{data.layout.b}</li> -->
-  <li>{data.page_server.a}</li>
-  <li>{data.page_server.b}</li>
-  {#each pprops as pp}
-    <li>{pp.id}</li>
-    <li>{pp.admin_name}</li>
-    <li>{pp.created_at}</li>
-  {/each}
-</ul>
-<br />
-<form method="POST" action="/?/login">
-  <label>
-    Email
-    <input name="id" type="text" />
-  </label>
-  <label>
-    Password
-    <input name="password" type="password" />
-  </label>
-  <button>Log in</button>
-</form>
-<br />
-<p>
-  form status:
-  {#if form?.success}
-    qwe
-  {:else}
-    false
-  {/if}
-</p>
 
+<p>home</p>
 <p>
   <span>Visit</span>
   <a class="rounded-lg bg-slate-400 p-1" href="https://kit.svelte.dev">kit.svelte.dev</a>
--- src/routes/+page.ts
+++
@@ -1,5 +0,0 @@
-// import type { PageLoad, PageServerData } from './$types';
-
-// export const load: PageLoad = () => {
-//   console.log('page.ts load');
-// };
